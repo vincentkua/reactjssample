552928af4dbd73d8fa9678f51ddd773bfce088ac
vincentkua <zylyn89@gmail.com>
2023-07-30 22:15:19 +0800

    testagain

---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+const Main = () => {
+    return ( 
+        <div>
+            <h3>Main page</h3>
+        </div>
+    );
+}
+ 
+export default Main;
---
+++ src/components/Page2.js
@@ -0,0 +1,9 @@
+const Page2 = () => {
+    return (  
+        <div>
+            <h3>Page2</h3>
+        </div>
+    );
+}
+ 
+export default Page2;
